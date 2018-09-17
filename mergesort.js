c2df78062e8a1cd13b30342ace7fddbb6e42af7e
Brittany Hill <brit.hill10@gmail.com>
2018-09-17 15:03:45 -0500

    Complete exercise

--- mergesort.js
+++ mergesort.js
@@ -10,32 +10,25 @@ function split(arr) {
     return [left, right];
 }
 
-function merge(left, right){
+function merge(left, right) {
     let sortedArray = [];
-    // if(left.length === 1 && right.length === 1){
-    //     if(right > left){
-    //         return left.concat(right);
-    //     }
-    //     else{
-    //         return right.concat(left);
-    //     }
-    // }
     let i = 0;
     let j = 0
-    while(i < left.length && j < right.length){
-        if(left[i] > right[j]){
+
+    while (i < left.length && j < right.length) {
+        if (left[i] > right[j]) {
             sortedArray.push(right[j])
             j++;
         }
-        else{
+        else {
             sortedArray.push(left[i])
             i++;
         }
     }
-    if(right.slice(j).length > 0){
+    if (right.slice(j).length > 0) {
         sortedArray.push(...right.slice(j));
     }
-    else{
+    else {
         sortedArray.push(...left.slice(i));
     }
 
@@ -43,9 +36,14 @@ function merge(left, right){
 
 }
 
-//1,4,7       2,5
-//1, 2
+function mergeSort(array) {
+    if (array.length === 1) {
+        return array;
+    } else {
+        let splitArr = split(array);
+        let left = mergeSort(splitArr[0]);
+        let right = mergeSort(splitArr[1]);
+        return merge(left, right)
 
-
-//3 4    2 5
-//
+    }
+  }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -13,7 +13,22 @@ describe('Merge function', function () {
        expect(merge([2],[1])).toEqual([1,2])
        expect(merge([1,2],[3,4])).toEqual([1,2,3,4])
        expect(merge([27,38],[3,43])).toEqual([3,27,38,43])
-       expect(merge([3,27,38,43],[9,10,82])).toEqual([3,9,10,27,38,43,82])
+       expect(merge([3,27,38,43],[9,10,82])).toEqual([3,9,10,27,38,43,82]);
+
+       
+
+
     });
 });
+
+describe('Merge Sort function', function () {
+    it('is able to merge two sorted arrays into one sorted array', function () {
+
+       expect(mergeSort([38, 27, 43, 3, 9, 82, 10])).toEqual([3, 9, 10, 27, 38, 43, 82]);
+       expect(mergeSort([2, 1])).toEqual([1, 2])
+
+
+    });
+});
+
 
